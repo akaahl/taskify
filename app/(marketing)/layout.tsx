50a1e7fa64c1ad3000d6656a77e816dc6bbb8bcc
Khair Afiq <a.k.afiq@live.com>
2024-10-02 19:37:35 +0800

    Create a Marketing page template

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function MarketingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="h-full bg-slate-100">
+      <main className="pt-40 pb-20 bg-slate-100">
+        {/* Navbar */}
+        {children}
+        {/* Footer */}
+      </main>
+    </div>
+  );
+}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-sky-500">Hwllo workd</h1>
-    </div>
-  );
-}
