1465972cf06067db249e5decf874413cbe8c3a92
Khair Afiq <a.k.afiq@live.com>
2024-10-04 03:08:00 +0800

    Configure Clerk middleware

--- app/(platform)/(dashboard)/_components/Navbar.tsx
+++ app/(platform)/(dashboard)/_components/Navbar.tsx
@@ -1,9 +1,11 @@
 import LogoHeader from "@/components/LogoHeader";
 import { Button } from "@/components/ui/button";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <nav className="fixed z-50 top-0 px-4 border-bw-full h-[4.5rem] shadow-sm bg-white flex items-center">
+    <nav className="fixed z-50 top-0 px-4 border-b w-full h-[4.5rem] shadow-sm bg-white flex items-center">
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <LogoHeader />
@@ -11,6 +13,39 @@ export default function Navbar() {
         <Button className="rounded-md hidden md:block h-auto py-1.5 px-2">
           Create
         </Button>
+        <Button className="rounded-sm block md:hidden ">
+          <Plus className="h-4 w-4" />
+        </Button>
+      </div>
+      <div className="ml-auto flex items-center gap-x-2">
+        <OrganizationSwitcher
+          hidePersonal
+          afterCreateOrganizationUrl={"/organization/:id"}
+          afterLeaveOrganizationUrl={"organization/:id"}
+          afterSelectOrganizationUrl={"/select-org"}
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "4.5rem",
+              },
+            },
+          }}
+        />
+        <UserButton
+          appearance={{
+            elements: {
+              rootBox: {
+                avatarBox: {
+                  height: 30,
+                  width: 30,
+                },
+              },
+            },
+          }}
+        />
       </div>
     </nav>
   );
