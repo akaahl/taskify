f0e47563ebbd0d8f01cc57332f7f51b9ce39e94e
Khair Afiq <a.k.afiq@live.com>
2024-10-06 20:17:09 +0800

    Fix bugs related to organizationId page, by adding a use client flag

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,26 @@
+"use client";
+
 import { createBoard } from "@/actions/createBoard";
+import { useAction } from "@/hooks/useActions";
 
 export default function OrganizationIdPage() {
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log("Success", data);
+    },
+    onError: (error) => {
+      console.log("Error!", error);
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    execute({ title });
+  };
+
   return (
     <div>
-      <form action={createBoard}>
+      <form action={onSubmit}>
         <input
           type="text"
           id="title"
