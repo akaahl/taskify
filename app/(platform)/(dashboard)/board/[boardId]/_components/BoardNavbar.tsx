9ea030bfacbe314654342794aed79dd550f13b00
Khair Afiq <a.k.afiq@live.com>
2024-10-10 03:13:51 +0800

    Create BoardTitleForm

--- app/(platform)/(dashboard)/board/[boardId]/_components/BoardNavbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/BoardNavbar.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/utils/db";
 import { auth } from "@clerk/nextjs/server";
 import { Board } from "@prisma/client";
+import BoardTitleForm from "./BoardTitleForm";
 
 interface BoardNavbarProps {
   data: Board;
@@ -9,7 +10,7 @@ interface BoardNavbarProps {
 export default async function BoardNavbar({ data }: BoardNavbarProps) {
   return (
     <div className="w-full h-14 z-[40] bg-black/50 fixed top-[4.5rem] flex items-center px-6 gap-x-4 text-white">
-      <h1>This is board navbar</h1>
+      <BoardTitleForm data={data} />
     </div>
   );
 }
