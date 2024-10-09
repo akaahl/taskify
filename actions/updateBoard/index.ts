77d9d79dbf247107b784cbbabdd1f50213bb67d8
Khair Afiq <a.k.afiq@live.com>
2024-10-10 03:22:53 +0800

    Create schema, types, and handler function for updating board title

---
+++ actions/updateBoard/index.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { InputType, ReturnType } from "./types";
+import prisma from "@/utils/db";
+import { revalidatePath } from "next/cache";
+import { createSafeActions } from "@/lib/createSafeActions";
+import { UpdateBoard } from "./schema";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { title, id } = data;
+
+  let board;
+
+  try {
+    board = await prisma.board.update({
+      where: {
+        id,
+        orgId,
+      },
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to update board",
+    };
+  }
+
+  revalidatePath(`/board/${board.id}`);
+
+  return { data: board };
+};
+
+export const updateBoard = createSafeActions(UpdateBoard, handler);
