d8c738a31a4dcdb7ff5301dbff2e98b3a999c729
Khair Afiq <a.k.afiq@live.com>
2024-10-11 19:22:40 +0800

    Create ListHeader and ListItem to render the list

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
@@ -2,6 +2,8 @@
 
 import { ListWithCards } from "@/types";
 import ListForm from "./ListForm";
+import { useEffect, useState } from "react";
+import ListItem from "./ListItem";
 
 interface ListContainerProps {
   boardId: string;
@@ -9,8 +11,21 @@ interface ListContainerProps {
 }
 
 export default function ListContainer({ boardId, data }: ListContainerProps) {
+  const [orderedData, setOrderedData] = useState(data);
+
+  useEffect(() => {
+    setOrderedData(data);
+  }, [data]);
+
   return (
-    <ol>
+    <ol className="flex gap-x-3 h-full">
+      {orderedData.map((list, index) => (
+        <ListItem
+          key={list.id}
+          index={index}
+          data={list}
+        />
+      ))}
       <ListForm />
       <div className="flex-shrink-0 w-1" />
     </ol>
