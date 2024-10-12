b87d10f43a6ed3696d28cbc24af58ca9d1255679
Khair Afiq <a.k.afiq@live.com>
2024-10-13 04:00:38 +0800

    Render FormTextArea if user wants to add a new card

--- app/(platform)/(dashboard)/board/[boardId]/_components/CardForm.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/CardForm.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import FormSubmit from "@/components/form/FormSubmit";
+import { FormTextArea } from "@/components/form/FormTextArea";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
 import { forwardRef } from "react";
 
 interface CardFormProps {
@@ -13,6 +15,38 @@ interface CardFormProps {
 
 export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
   ({ listId, isEditing, enableEditing, disableEditing }, ref) => {
+    if (isEditing) {
+      return (
+        <form
+          action=""
+          className="m1 py-0.5 px-1 space-y-4"
+        >
+          <FormTextArea
+            id="title"
+            ref={ref}
+            onKeyDown={() => {}}
+            placeholder="Enter a title for this card"
+          />
+          <input
+            hidden
+            name="listId"
+            id="listId"
+            defaultValue={listId}
+          />
+          <div className="flex items-center gap-x-1">
+            <FormSubmit>Add card</FormSubmit>
+            <Button
+              size="sm"
+              variant="ghost"
+              onClick={disableEditing}
+            >
+              <X className="h-5 w-5" />
+            </Button>
+          </div>
+        </form>
+      );
+    }
+
     return (
       <div className="pt-2 px-2">
         <Button
@@ -20,6 +54,7 @@ export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
           text-muted-foreground text-sm"
           size="sm"
           variant="ghost"
+          onClick={enableEditing}
         >
           <Plus className="mr-2 h-4 w-4" />
           Add a new card
