b1a6f43d40bd2480a19b027b1ff39e0e1aa3f6e1
Khair Afiq <a.k.afiq@live.com>
2024-10-14 03:13:06 +0800

    Create server actions to save the updated and reordered lists

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
@@ -5,6 +5,9 @@ import ListForm from "./ListForm";
 import { useEffect, useState } from "react";
 import ListItem from "./ListItem";
 import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+import { useAction } from "@/hooks/useActions";
+import { updateListOrder } from "@/actions/updateListOrder";
+import { toast } from "sonner";
 
 interface ListContainerProps {
   boardId: string;
@@ -21,6 +24,15 @@ function reorder<T>(list: T[], startIndex: number, endIndex: number) {
 
 export default function ListContainer({ boardId, data }: ListContainerProps) {
   const [orderedData, setOrderedData] = useState(data);
+
+  const { execute: executeUpdateListOrder } = useAction(updateListOrder, {
+    onSuccess: () => {
+      toast.success("List reordered!");
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
 
   const onDragEnd = (result: any) => {
     const { destination, source, type } = result;
@@ -41,6 +53,7 @@ export default function ListContainer({ boardId, data }: ListContainerProps) {
         (item, index) => ({ ...item, order: index }),
       );
       setOrderedData(items);
+      executeUpdateListOrder({ items, boardId });
     }
 
     if (type === "card") {
