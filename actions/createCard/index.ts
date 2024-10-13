cd956af4ac35081a842adb7b97d213ff760d5b44
Khair Afiq <a.k.afiq@live.com>
2024-10-13 18:57:00 +0800

    Create the schema, types, and actions for adding a new card

---
+++ actions/createCard/index.ts
@@ -0,0 +1,67 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { InputType, ReturnType } from "./types";
+import prisma from "@/utils/db";
+import { revalidatePath } from "next/cache";
+import { createSafeActions } from "@/lib/createSafeActions";
+import { CreateCard } from "./schema";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { title, listId, boardId } = data;
+
+  let card;
+
+  try {
+    const list = await prisma.list.findUnique({
+      where: {
+        id: listId,
+        board: {
+          orgId,
+        },
+      },
+    });
+
+    if (!list) {
+      return {
+        error: "List not found",
+      };
+    }
+
+    const lastCard = await prisma.card.findFirst({
+      where: {
+        listId,
+      },
+      orderBy: { order: "desc" },
+      select: { order: true },
+    });
+
+    const newOrder = lastCard ? lastCard.order + 1 : 1;
+
+    card = await prisma.card.create({
+      data: {
+        title,
+        listId,
+        order: newOrder,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to create a new card.",
+    };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+
+  return { data: card };
+};
+
+export const createCard = createSafeActions(CreateCard, handler);
--- app/(platform)/(dashboard)/board/[boardId]/_components/CardForm.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/CardForm.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import { createCard } from "@/actions/createCard";
 import FormSubmit from "@/components/form/FormSubmit";
 import { FormTextArea } from "@/components/form/FormTextArea";
 import { Button } from "@/components/ui/button";
+import { useAction } from "@/hooks/useActions";
 import { Plus, X } from "lucide-react";
-import { forwardRef } from "react";
+import { useParams } from "next/navigation";
+import { ElementRef, forwardRef, KeyboardEventHandler, useRef } from "react";
+import { toast } from "sonner";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
 
 interface CardFormProps {
   listId: string;
@@ -15,17 +20,59 @@ interface CardFormProps {
 
 export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
   ({ listId, isEditing, enableEditing, disableEditing }, ref) => {
+    const params = useParams();
+    const formRef = useRef<ElementRef<"form">>(null);
+
+    const { execute, fieldErrors } = useAction(createCard, {
+      onSuccess: (result) => {
+        toast.success(`New card "${result.title}" has been created.`);
+        formRef.current?.reset();
+        disableEditing();
+      },
+      onError: (error) => {
+        toast.error(error);
+      },
+    });
+
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        disableEditing();
+      }
+    };
+
+    useOnClickOutside(formRef, disableEditing);
+    useEventListener("keydown", onKeyDown);
+
+    const onTextAreaKeyDown: KeyboardEventHandler<HTMLTextAreaElement> = (
+      e,
+    ) => {
+      if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        formRef.current?.requestSubmit();
+      }
+    };
+
+    const onSubmit = (formData: FormData) => {
+      const title = formData.get("title") as string;
+      const listId = formData.get("listId") as string;
+      const boardId = params.boardId as string;
+
+      execute({ title, listId, boardId });
+    };
+
     if (isEditing) {
       return (
         <form
-          action=""
+          ref={formRef}
+          action={onSubmit}
           className="m1 py-0.5 px-1 space-y-4"
         >
           <FormTextArea
             id="title"
             ref={ref}
-            onKeyDown={() => {}}
+            onKeyDown={onTextAreaKeyDown}
             placeholder="Enter a title for this card"
+            errors={fieldErrors}
           />
           <input
             hidden
