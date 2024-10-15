70a4b1b0665ce60cc5079e43a8a15033c40ab908
Khair Afiq <a.k.afiq@live.com>
2024-10-16 03:14:46 +0800

    Create several functions and states within the Description component to submit the form event

--- components/modals/cardModal/Description.tsx
+++ components/modals/cardModal/Description.tsx
@@ -1,21 +1,81 @@
 "use client";
 
+import { FormInput } from "@/components/form/FormInput";
+import { FormTextArea } from "@/components/form/FormTextArea";
 import { Skeleton } from "@/components/ui/skeleton";
 import { CardWithList } from "@/types";
+import { useQueryClient } from "@tanstack/react-query";
 import { AlignLeft } from "lucide-react";
+import { useParams } from "next/navigation";
+import { ElementRef, useRef, useState } from "react";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
 
 interface DescriptionProps {
   data: CardWithList;
 }
 
 export const Description = ({ data }: DescriptionProps) => {
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const params = useParams();
+  const queryClient = useQueryClient();
+
+  const formRef = useRef<ElementRef<"form">>(null);
+  const textAreaRef = useRef<ElementRef<"textarea">>(null);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      textAreaRef.current?.focus();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      disableEditing();
+    }
+  };
+
+  useEventListener("keydown", onKeyDown);
+  useOnClickOutside(formRef, disableEditing);
+
+  const onSubmit = (formData: FormData) => {
+    const description = formData.get("description") as string;
+    const boardId = params.boardId as string;
+  };
+
   return (
     <div className="flex items-start gap-x-3 w-full">
       <AlignLeft className="h-5 w-5 mt-0.5 text-neutral-700" />
       <div className="w-full">
         <p className="font-semibold text-neutral-700 mb-2">Description</p>
+        {isEditing ? (
+          <form
+            action=""
+            ref={formRef}
+            className="space-y-2"
+          >
+            <FormTextArea
+              id="description"
+              className="w-full mt-2"
+              placeholder="Add a more detailed description"
+              defaultValue={data.description || undefined}
+            />
+          </form>
+        ) : (
+          <div
+            role="button"
+            className="min-h-[78px] bg-neutral-200 text-sm font-medium py-3 
+            px-3.5 rounded-md"
+            onClick={enableEditing}
+          >
+            {data.description || "Add a more detailed description"}
+          </div>
+        )}
       </div>
-      {data.title}
     </div>
   );
 };
